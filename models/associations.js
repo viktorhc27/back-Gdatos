c5dea9ce117939f28e8f979159f029eb069fb976
viktorhc27 <victor.huanca1999@gmail.com>
2023-12-22 19:31:21 -0300

    cf

--- models/associations.js
+++ models/associations.js
@@ -1,20 +1,20 @@
 const Producto = require('./Productos')
 const Categoria = require('./Categoria')
 const Ventas = require('./Ventas')
-const DetalleVenta = require('./detalle_venta')
+//const DetalleVenta = require('./detalle_venta')
 const MetodoPagos = require('./metodo_pagos')
 const TipoDocumento = require('./tipo_documento')
 
 Categoria.hasMany(Producto, { foreignKey: 'categoria_id' })
 Producto.belongsTo(Categoria, { foreignKey: 'categoria_id', as: 'categoria' })
 
-
+/* 
 Ventas.hasMany(DetalleVenta, { foreignKey: 'venta_id' });
 DetalleVenta.belongsTo(Ventas, { foreignKey: 'venta_id' });
 
 Producto.hasMany(DetalleVenta, { foreignKey: 'producto_id' });
 DetalleVenta.belongsTo(Producto, { foreignKey: 'producto_id' });
-
+ */
 MetodoPagos.hasOne(Ventas, { foreignKey: 'metodo_pagos_id' })
 Ventas.belongsTo(MetodoPagos, { foreignKey: 'metodo_pagos_id' })
 
@@ -25,7 +25,7 @@ module.exports = {
     Producto,
     Categoria,
     Ventas,
-    DetalleVenta,
+   // DetalleVenta,
     MetodoPagos,
     TipoDocumento
 }
